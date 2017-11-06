1c6d8ea4b9dfdbb683006d12a38d979773e5d264
mweidlut <mweidlut@163.com>
2017-11-07 00:02:26 +0800

    added eureka feign

---
+++ EurekaFeignLB/src/main/java/EurekaFeignLBApplication.java
@@ -0,0 +1,21 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * User: ROOT
+ * Date: 2017/11/6 22:03
+ */
+@ComponentScan("org.test.cloud")
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class EurekaFeignLBApplication {
+
+    public void main(String[] args){
+        SpringApplication.run(EurekaFeignLBApplication.class, args);
+    }
+
+}
---
+++ EurekaFeignLB/src/main/java/org/test/cloud/PersonFeignClient.java
@@ -0,0 +1,19 @@
+package org.test.cloud;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * User: ROOT
+ * Date: 2017/11/6 22:07
+ */
+@FeignClient(value = "eureka-client")
+public interface PersonFeignClient {
+
+    @RequestMapping(method = RequestMethod.GET, value = "/person")
+    String person(@RequestParam(value = "firstname") String firstname,
+                  @RequestParam(value = "lastname") String lastname);
+
+}
