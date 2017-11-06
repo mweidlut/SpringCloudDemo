940b8f963d808753e571c37dc56c58f1b9865f18
mweidlut <mweidlut@163.com>
2017-11-06 21:46:28 +0800

    modified ribbon lb

--- EurekaRibbonLB/src/main/java/EurekaRibbonLBApplication.java
+++ EurekaRibbonLB/src/main/java/EurekaRibbonLBApplication.java
@@ -4,14 +4,12 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 /**
  * User: ROOT
  * Date: 2017/11/1 21:50
  */
-@Configuration
 @ComponentScan("org.test.cloud")
 @SpringCloudApplication
 @EnableDiscoveryClient
